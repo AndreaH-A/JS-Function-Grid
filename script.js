d1323f22584ad84f65ccec75da61459d864c1e90
Andrea Hernandez Aguilar <andrea.hdz.aguilar@gmail.com>
2017-01-17 23:26:30 +0000

    my comment

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //Write your function calls below this line
+setColor(0,0,"purple");
+setColor(0,9,"purple");
+setColor(9,9,"purple");
+setColor(9,0,"purple");
+setColor(3,3,"blue");
+setColor(3,6,"blue");
+setColor(2,6,"blue");
+setColor(2,3,"blue");
+setColor(6,3,"blue");
+setColor(7,4,"blue");
+setColor(7,5,"blue");
+setColor(6,6,"blue");
+setColor(3,0,"green");
